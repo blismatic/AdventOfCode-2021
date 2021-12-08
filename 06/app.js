0dc14f4e4a280a463ded602827f2545c74caec63
Seth Collins <blismatic@gmail.com>
2021-12-07 19:49:33 -0800

    finished day 6... very interesting solution to part 2

--- 06/app.js
+++ 06/app.js
@@ -1,28 +1,49 @@
 const fs = require('fs');
-let data = fs.readFileSync('./06/test.txt', 'utf-8');
+let data = fs.readFileSync('./06/input.txt', 'utf-8');
 data = data.split(",");
 //console.log(data);
 
+// Part 1
 function iterateDays(numOfDays, arr) {
     //console.log(`Initial state: ${arr}`);
     for (let i = 0; i < numOfDays; i++) {
         for (let j = 0; j < arr.length; j++) {
             let currentFish = arr[j];
-            //console.log(`currentFish: ${currentFish}`);
-
             if (currentFish == 0) {
                 currentFish = 6;
                 arr[j] = currentFish;
                 arr.push(9);
             } else {
-                //console.log(`subtracting 1 from ${currentFish}`);
                 currentFish -= 1;
                 arr[j] = currentFish;
             }
         }
-        //console.log(`After ${i+1} days: ${arr}`);
     }
     return arr
 }
 
-console.log(iterateDays(256, data).length);
+//let newData = iterateDays(80, data);
+//console.log(newData.length);
+console.log(simulate(80, data));
+
+// Part 2
+function simulate(numOfDays, arr) {
+    // initialize buckets representing each age group of fish
+    let fish = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+    // assign each fish to its corresponding bucket
+    //console.log(`fish before: ${fish}`);
+    arr.forEach(x => fish[Number(x)]++);
+    //console.log(`fish after: ${fish}`);
+
+    // adjust values in buckets based on previous amounts
+    for (let day = 0; day < numOfDays; day++) {
+        let newFish = fish.shift();
+        fish[6] += newFish;
+        fish.push(newFish)
+    }
+
+    return fish.reduce((sum, x) => sum + x);
+}
+
+console.log(simulate(256, data));
